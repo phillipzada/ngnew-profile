109c8be19f4dccb686c3f4548ca71d3ada6f3fd5
Phillip Zada <phillip@zware.com.au>
2017-12-02 21:14:51 +1100

    step 19 async testing

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,10 +1,15 @@
+
 import { MaterialModule } from '../lib/material/material.module';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { HomeService } from './home.service';
 import { of } from 'rxjs/observable/of';
+
+import { map, take } from 'rxjs/operators';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { async as scheduler } from 'rxjs/scheduler/async';
+import { timer } from 'rxjs/observable/timer';
 
 describe(`Component: Home Component`, () => {
     let component: HomeComponent = null;
@@ -53,6 +58,73 @@ describe(`Component: Home Component`, () => {
         const el = de.query(By.css('h1')).nativeElement;
 
         expect(el.innerText).toEqual('FAKE MESSAGE');
+    });
+
+    it(`should get next message on click - with fakeAsync`, fakeAsync(() => {
+        spyOn(homeService, 'getMessage')
+            .and.returnValues(
+            of('FAKE MESSAGE 1'),
+            timer(2000, scheduler).pipe(take(1), map(x => 'FAKE MESSAGE 2')));
+
+        fixture.detectChanges();
+
+        const el = de.query(By.css('h1')).nativeElement;
+
+        expect(el.innerText).toEqual('FAKE MESSAGE 1');
+
+        const button = fixture.debugElement.query(By.css('button')).nativeElement;
+        button.click();
+
+        tick(3000);
+
+        fixture.detectChanges();
+
+        expect(el.innerText).toEqual('FAKE MESSAGE 2');
+    }));
+
+    it(`should get next message on click - with async`, async(() => {
+        spyOn(homeService, 'getMessage')
+            .and.returnValues(
+            of('FAKE MESSAGE 1'),
+            timer(2000, scheduler).pipe(take(1), map(x => 'FAKE MESSAGE 2')));
+
+        fixture.detectChanges();
+
+        const el = de.query(By.css('h1')).nativeElement;
+
+        expect(el.innerText).toEqual('FAKE MESSAGE 1');
+
+        const button = fixture.debugElement.query(By.css('button')).nativeElement;
+        button.click();
+
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
+            expect(el.innerText).toEqual('FAKE MESSAGE 2');
+        });
+
+    }));
+
+    it('should get next message on click - with done', (done) => {
+        const spy = spyOn(homeService, 'getMessage')
+            .and.returnValues(
+            of('FAKE MESSAGE 1'),
+            timer(2000, scheduler).pipe(take(1), map(x => 'FAKE MESSAGE 2')));
+
+        fixture.detectChanges();
+        const el = de.query(By.css('h1')).nativeElement;
+
+        expect(el.innerText).toEqual('FAKE MESSAGE 1');
+
+        const button = fixture.debugElement.query(By.css('button')).nativeElement;
+        button.click();
+
+        spy.calls.mostRecent().returnValue
+            .subscribe(() => {
+                fixture.detectChanges();
+                expect(el.innerText).toEqual('FAKE MESSAGE 2');
+                done();
+            });
+
     });
 
 });
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -4,10 +4,21 @@ import { of } from 'rxjs/observable/of';
 @Injectable()
 export class HomeService {
 
+  private messages = [
+    'Routing & Lazy Loading Rocks!',
+    'Angular Testing is too cool',
+    'This training course is awesome'
+  ];
+  private index = 0;
+
   constructor() { }
 
   getMessage() {
-    return of('Routing &amp; Lazy Loading Rocks!');
+    const currentMessage = this.messages[this.index++];
+    if (this.index > this.messages.length - 1) {
+      this.index = 0;
+    }
+    return of(currentMessage);
   }
 
 }
